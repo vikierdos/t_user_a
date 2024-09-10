6e95c37997cb1300382ab68e6977ff564233d394
vikierdos <csicsorka@gmail.com>
2024-09-10 15:45:04 +0200

    projects_done

---
+++ education/app/Models/Project.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'end_date',
+        'status',
+        'user_id',
+        'project_id'
+    ];
+}
